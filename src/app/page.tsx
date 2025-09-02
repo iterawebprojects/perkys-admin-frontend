950c256b810abd02b16c9664de0b0197fc9ed88b
iterawebprojects <iterawebprojects@gmail.com>
2025-09-02 19:30:55 -0300

    style: correct format and code style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
+import { Button } from "@/components/ui/button";
+
 function HomePage() {
   return (
     <main>
-      <section className="flex flex-col gap-y-2 p-5">
-        <h1 className="text-4xl font-bold text-center">
+      <section className="flex flex-col items-center justify-center gap-y-2 p-5">
+        <h1 className="text-center font-bold text-4xl">
           Welcome to the Home Page
         </h1>
+        <Button>Click me</Button>
       </section>
     </main>
   );
