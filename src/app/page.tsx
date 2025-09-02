e38408d9315551f74bcde8e3f98b8a0a58fceb85
davidmedev222 <davidmedev@gmail.com>
2025-09-02 13:17:22 -0300

    chore: first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+function HomePage() {
+  return (
+    <main>
+      <section className="flex flex-col gap-y-2 p-5">
+        <h1 className="text-4xl font-bold text-center">
+          Welcome to the Home Page
+        </h1>
+      </section>
+    </main>
+  );
+}
+
+export default HomePage;
